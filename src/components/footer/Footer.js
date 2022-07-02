803b5f83dcf5e610f3066a65c4e91b7a43757384
Lauriacunia <lauriacunia@gmail.com>
2022-07-02 11:44:45 -0300

    Avanzo con maquetado de footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,37 @@
 import React from "react";
-
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Container from '@mui/material/Container';
 
 const Footer = () => {
     return (
-        <div >Soy footer</div>
+         <Container  maxWidth={false}>
+            <Typography
+                variant="subtitle1" gutterBottom>
+                {"Sitio realizado por Laura Acuña"}
+            </Typography>
+            <Typography
+                variant="subtitle3" gutterBottom>
+                {"FullStack developer"}
+            </Typography>
+            <div >
+                <a href="https://github.com/Lauriacunia" target="_blank" rel="noreferrer">
+                    <IconButton 
+                        aria-label="add an alarm">
+                        <GitHubIcon />
+                    </IconButton>
+                </a>
+                <a href="https://www.linkedin.com/in/lauriacunia/" target="_blank" rel="noreferrer">
+                    <IconButton 
+                        aria-label="add an alarm">
+                        <LinkedInIcon  />
+                    </IconButton>
+                </a>
+
+            </div>
+        </Container>
     )
 }
 
