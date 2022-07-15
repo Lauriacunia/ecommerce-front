316ba0ba898bc6ab03e7130ffda97f5b85dfcfe4
Lauriacunia <lauriacunia@gmail.com>
2022-07-15 12:50:02 -0300

    Agrego forms de login y registro

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,6 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import FormControl from "@mui/material/FormControl";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import FormHelperText from "@mui/material/FormHelperText";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 const Login = () => {
-    return <div>Login</div>;
-    }
-export default Login; 
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container
+        maxWidth="sm"
+        sx={{ bgcolor: "#cfe8fc", padding: "50px", minHeight: "800px" }}
+      >
+        <Grid container spacing={2} justifyContent="">
+          <Grid item>
+            <FormControl sx={{ m: 1, width: "350px", marginBottom: "20px" }}>
+              <InputLabel htmlFor="my-input">Email</InputLabel>
+              <Input id="email" aria-describedby="my-helper-text" />
+            </FormControl>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} justifyContent="space-between">
+          <Grid item>
+            <FormControl sx={{ m: 1, width: "350px", marginBottom: "20px" }}>
+              <InputLabel htmlFor="my-input">Contraseña</InputLabel>
+              <Input id="my-input" aria-describedby="my-helper-text" />
+            </FormControl>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} justifyContent="flex-end">
+          <Grid item>
+            <Button sx={{ marginTop: "100px" }} variant="outlined">
+              Ingresar
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} justifyContent="flex-end">
+          <Grid item>
+            <Button size="small" sx={{ marginTop: "100px" }} color="secondary">
+              Quiero Registrarme
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
+};
+export default Login;
